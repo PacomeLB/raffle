1bb5ca382d09819c51d4ad969494160434bf3dfa
PacomeLB <39722461+PacomeLB@users.noreply.github.com>
2025-05-12 19:14:12 +0200

    enhance pool info reading

--- README.md
+++ README.md
@@ -80,3 +80,28 @@ forge script --chain sepolia script/RaffleAdminPayTaxVrf.s.sol:RaffleAdminPayTax
 forge script --chain sepolia script/SwapERC20ToEthUniSwapV4.s.sol:SwapERC20ToEthUniSwapV4Script --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv --interactives 1
 
 https://sepolia.etherscan.io/tx/0xde655055445eb49b3e189cdef2b5fccc87a1b11bbb7ea4a8d5192a717e5a8141/advanced
+
+# fork sepolia to local
+
+anvil --fork-url https://eth-sepolia.g.alchemy.com/v2/GBm__wLDvs_zeYjXn7L5Krw2RXBHWMeO
+
+# Usage of cast for debuging
+
+export swap=0x4dD3f964dC618d58569f70a1C8f57905D94bc4e0 // Swap contract address
+export paco=0x7a79A7c9338032B116051e9CC4459600F95fc35E // perso wallet address
+export unlucky=0xca4365A099eE5E9f20a1Dc8d325Ae8751A0cd87F // random wallet that has link
+export link=0x779877A7B0D9E8603169DdbD7836e478b4624789 // Link token address
+
+cast call $link "balanceOf(address)(uint256)" $paco // Call view / pure function on $link contract
+
+cast rpc anvil_impersonateAccount $paco // get control of account $paco in the fork
+cast rpc anvil_impersonateAccount $unlucky // get control of account $unlucky in the fork
+
+cast send $link --from $unlucky "transfer(address,uint256)(bool)" $paco 29000000000000000000 --unlocked // Send 29000000000000000000 Link token from $unlucky to paco
+
+// use fonction payWithLink on contract $swap
+// send to create a transaction, --unlocked to use controller account from anvil_impersonateAccount
+cast send $swap --from $paco --unlocked "payWithLink(bytes, address, uint256, uint256, uint256)" 0x64cdd75b9e2bcb6cc6474b6f23a0a39b3b707d362dc74a566fa2019cfd1f23b20dc6c95e44e56f0822c43571061c4188829702ee571001c734f1f15ae12a00d81b 0x7a79A7c9338032B116051e9CC4459600F95fc35E 1000000000000000000 1 1746716465
+
+// Debug transaction 0xf6efcd00ca724eed0177fd2eb276d597c11a12bb2ad92bca9dccb628954cbd31 from the fork
+cast run --debug 0xf6efcd00ca724eed0177fd2eb276d597c11a12bb2ad92bca9dccb628954cbd31
--- src/PoolStateReader.sol
+++ src/PoolStateReader.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.26;
 
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 
 contract PoolStateReader {
     using PoolIdLibrary for PoolKey;
@@ -16,6 +18,16 @@ contract PoolStateReader {
         uint256 protocolFee,
         uint256 lpFee
     );
+
+
+
+    struct PoolInfo {
+        PoolKey key;
+        uint160 sqrtPriceX96;
+        int24 tick;
+        uint24 protocolFee;
+        uint24 lpFee;
+    }
 
     IPoolManager public immutable poolManager;
 
@@ -30,6 +42,74 @@ contract PoolStateReader {
     uint24 lpFee){
     
         return poolManager.getSlot0(key.toId());
+    }
+
+    /** Get all pool states for a given ERC20 token
+     * @param _tokenERC20 The address of the ERC20 token
+     * @return poolInfos An array of PoolInfo structs containing the state of each pool with fees 500, 3000, and 10000
+     */
+     function getPoolStateERC20ToEth(address _tokenERC20) public view returns (PoolInfo[] memory poolInfos){
+
+        // Instanciate the array of PoolInfo structs with a length of 3
+        poolInfos = new PoolInfo[](3);
+
+        // Fee 500
+        PoolKey memory key = getPoolKey(_tokenERC20, 500, 10);
+
+        (uint160 sqrtPriceX96, int24 tick, uint24 protocolFee, uint24 lpFee)  = poolManager.getSlot0(key.toId());
+
+        poolInfos[0] = PoolInfo({
+            key: key,
+            sqrtPriceX96: sqrtPriceX96,
+            tick: tick,
+            protocolFee: protocolFee,
+            lpFee: lpFee
+        });
+
+        // Fee 3000
+        key =getPoolKey(_tokenERC20, 3000, 60);
+
+        (sqrtPriceX96, tick, protocolFee, lpFee)  = poolManager.getSlot0(key.toId());
+
+        poolInfos[1] = PoolInfo({
+            key: key,
+            sqrtPriceX96: sqrtPriceX96,
+            tick: tick,
+            protocolFee: protocolFee,
+            lpFee: lpFee
+        });
+
+        // Fee 10000
+        key = getPoolKey(_tokenERC20, 10000, 200);
+
+        (sqrtPriceX96, tick, protocolFee, lpFee)  = poolManager.getSlot0(key.toId());
+
+        poolInfos[2] = PoolInfo({
+            key: key,
+            sqrtPriceX96: sqrtPriceX96,
+            tick: tick,
+            protocolFee: protocolFee,
+            lpFee: lpFee
+        });
+
+        return poolInfos;
+        
+    }
+
+    /** Create a pool key for a given ERC20 token
+     * @param _tokenERC20 The address of the ERC20 token
+     * @param _fee The fee tier for the pool
+     * @param _tickSpacing The tick spacing for the pool
+     * @return poolKey The PoolKey struct for the pool
+     */
+    function getPoolKey(address _tokenERC20, uint24 _fee, int24 _tickSpacing) private pure returns (PoolKey memory) {
+        return PoolKey({
+            currency0: CurrencyLibrary.ADDRESS_ZERO,
+            currency1: Currency.wrap(_tokenERC20),
+            fee: _fee,
+            tickSpacing: _tickSpacing,
+            hooks: IHooks(address(0))
+        });
     }
 
     function emitPoolState(PoolKey calldata key) internal {
